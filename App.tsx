a84d1e714ad3d2c5173c94bedbba69baf451701b
RohanMukka <rohanmukka09@gmail.com>
2025-06-06 10:13:10 -0500

    change theme

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ import Footer from './components/common/Footer';
 const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [activeSection, setActiveSection] = useState('home');
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(true); // Default to dark mode
 
   const homeRef = useRef<HTMLElement | null>(null);
   const aboutRef = useRef<HTMLElement | null>(null);
