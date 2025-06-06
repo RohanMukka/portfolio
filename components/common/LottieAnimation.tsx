b843de43a5dcd466b0e9f4374ee1b5e790f7833a
RohanMukka <rohanmukka09@gmail.com>
2025-06-05 22:04:00 -0500

    Revert "Merge pull request #10 from RohanMukka/rohan/install-tailwind,-react,-and-lottie-dependencies"
    This reverts commit 869208bf0488178e5fceee39af8feaa40b75556d, reversing
    changes made to 5c86725da1e5f5476ce679ce880a5664ff43b0f1.

--- components/common/LottieAnimation.tsx
+++ components/common/LottieAnimation.tsx
@@ -1,24 +1,31 @@
 
 import React, { useEffect, useRef } from 'react';
 import { LottieAnimationProps } from '../../types';
-import lottie from 'lottie-web';
+
+declare global {
+  interface Window {
+    lottie?: {
+      loadAnimation: (options: any) => any; // Simplified type for lottie
+    };
+  }
+}
 
 const LottieAnimation: React.FC<LottieAnimationProps> = ({ animationData, loop, className }) => {
   const animationContainer = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    let anim: any;
-    if (animationContainer.current) {
-      anim = lottie.loadAnimation({
+    let anim: any; 
+    if (typeof window !== 'undefined' && window.lottie && animationContainer.current) {
+      anim = window.lottie.loadAnimation({
         container: animationContainer.current,
         renderer: 'svg',
         loop: loop !== undefined ? loop : true,
         autoplay: true,
-        animationData,
+        animationData: animationData,
       });
     }
     return () => {
-      anim?.destroy();
+      anim?.destroy(); 
     };
   }, [animationData, loop]);
 
